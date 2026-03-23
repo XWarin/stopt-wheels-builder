e497292a809e5d8706725a70bebee49cab69c6db
c97830 <xavier.warin@edf.fr>
2026-03-23 20:23:59 +0100

    NoTest

--- python/pystopt/__init__.py
+++ python/pystopt/__init__.py
@@ -1,6 +1,10 @@
-from . import grids
-from . import cdf
-from . import tree
-from . import regression
-from . import geners
-from . import glob
+from .pystopt import *   # charge l’extension C++
+import importlib
+
+# expose les sous modules C++ comme modules Python
+grids = importlib.import_module("pystopt.grids")
+tree = importlib.import_module("pystopt.tree")
+cdf = importlib.import_module("pystopt.cdf")
+regression = importlib.import_module("pystopt.regression")
+geners = importlib.import_module("pystopt.geners")
+glob = importlib.import_module("pystopt.glob")
